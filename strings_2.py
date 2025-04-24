8230e75470092a17eb090674ac7f9c22f404193a
natalieloshak <natalil1204@gmail.com>
2025-04-24 18:20:54 +0300

    input practice

--- strings_2.py
+++ strings_2.py
@@ -1,12 +1,11 @@
-name = "Наталія"
+import templates_practice
+
+name = input(templates_practice.MSG_ENTER_NAME)
+# name = "Наталія"
 address = "проспект С.Бандери 26Б"
 service = "манікюр з покриттям"
 time = "25 квітня о 15:00"
 smile1 = "💅"
 smile2 = "❤"
 sms = f"Шановна {name}, ми Вас очікуємо на {service} {smile1}. \nАдреса:{address}. \nДата: {time}. \nДо зустрічі! {smile2}"
-
-import templates_practice
-
 sms2 = templates_practice.SMS_WARNING_MSG.format(name=name, service=service, time=time)
-pass

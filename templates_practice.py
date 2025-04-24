9e1459f01afb7098a57661d5e76af429ebbcfdaf
natalieloshak <natalil1204@gmail.com>
2025-04-24 18:20:38 +0300

    Templates practice, part2

--- templates_practice.py
+++ templates_practice.py
@@ -1,3 +1,5 @@
 SMS_TEMPLATE = "Шановна {name}, ми Вас очікуємо на {service} {smile1}. Адреса:{address}. Дата: {time}. До зустрічі! {smile2}"
 
 SMS_WARNING_MSG = "{name}, очікуємо Вас на {service}. Дата: {time}"
+
+MSG_ENTER_NAME = "Введіть Ваше ім'я:"
